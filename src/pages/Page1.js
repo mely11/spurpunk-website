309de565af648495225002bab1d12f167d8f1a28
anjalimasur <amasur@usc.edu>
2024-11-11 18:23:23 -0500

    Update Page1.js

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -27,6 +27,26 @@ import PlayTrailerArrow from '../assets/images/Play-Trailer-Arrow@.png';
 // Importing video
 import GameplayVideo from '../assets/videos/Gameplay_BurialGrounds_Clips_Better.mp4';
 
+// Page2
+import AllText from '../assets/images/All_text@.png'
+import Page2DottedLine from '../assets/images/Page-2-dotted_line@.png'
+import HeroFrame from '../assets/images/Hero_frame@.png'
+import TextBackground from '../assets/images/Page2_Rectangle_1@.png'
+import AllTextBackground from '../assets/images/Page2-Rectangle@.png'
+import Sean from '../assets/images/Hero_Sean@.png'
+import Margarita from '../assets/images/Hero_Margarita@.png'
+import Phil from '../assets/images/Hero_Phil@.png'
+import Mickey from '../assets/images/Hero_Mickey@.png'
+import SeanGearSilhouette from '../assets/images/Page2_Phil_Gear_silhouette@.png'
+import SeanGear from '../assets/images/Page2_Phil_Gear@.png'
+import MargaritaCardsSilhouette from '../assets/images/Page2_Susies_Cards_silhouette@.png'
+import MargaritaCards from '../assets/images/Page2_Susies_Cards@.png'
+import PhilBombSilhouette from '../assets/images/Page2_Phils_Bomb_silhouette@.png'
+import PhilBomb from '../assets/images/Page2_Phils_Bomb@.png'
+import MickeySkullSilhouette from '../assets/images/Page2_Mickeys_Skull_silhouette@.png'
+import MickeySkull from '../assets/images/Page2_Mickeys_Skull@.png'
+
+
 const Page1 = () => {
   return (
     <div className="page1">
@@ -208,6 +228,227 @@ const Page1 = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
       />
+
+    <div>
+
+    {/* All text */}
+    <motion.img
+      src={AllText}
+      alt="Meet Your Instructors"
+      className="all-text"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* All text background */}
+    <motion.img
+      src={AllTextBackground}
+      alt="All Text Background"
+      className="all-text-background"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Text BG 1 */}
+    <motion.img
+      src={TextBackground}
+      alt="Text Background"
+      className="text-background1"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Text BG 2 */}
+    <motion.img
+      src={TextBackground}
+      alt="Text Background"
+      className="text-background2"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Text BG 3 */}
+    <motion.img
+      src={TextBackground}
+      alt="Text Background"
+      className="text-background3"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Text BG 4 */}
+    <motion.img
+      src={TextBackground}
+      alt="Text Background"
+      className="text-background4"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.625, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Page 2 Dotted Line */}
+    <motion.img
+      src={Page2DottedLine}
+      alt="Page 2 Dotted Line"
+      className="page2-dotted-line"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Hero frame 1 */}
+    <motion.img
+      src={HeroFrame}
+      alt="Hero frame 1"
+      className="hero-frame1"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Hero frame 2 */}
+    <motion.img
+      src={HeroFrame}
+      alt="Hero frame 2"
+      className="hero-frame2"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Hero frame 3 */}
+    <motion.img
+      src={HeroFrame}
+      alt="Hero frame 3"
+      className="hero-frame3"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Hero frame 4 */}
+    <motion.img
+      src={HeroFrame}
+      alt="Hero frame 4"
+      className="hero-frame4"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Sean */}
+    <motion.img
+      src={Sean}
+      alt="Hero Sean"
+      className="sean"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Margarita */}
+    <motion.img
+      src={Margarita}
+      alt="Hero Margarita"
+      className="margarita"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Phil */}
+    <motion.img
+      src={Phil}
+      alt="Hero Phil"
+      className="phil"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={Mickey}
+      alt="Hero Mickey"
+      className="mickey"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    {/* Sean's gear silhouette*/}
+    <motion.img
+      src={SeanGearSilhouette}
+      alt="Sean's gear silhouette"
+      className="sean-gear-silhouette"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={SeanGear}
+      alt="Sean's gear"
+      className="sean-gear"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={MargaritaCardsSilhouette}
+      alt="Margarita's cards silhouette"
+      className="margarita-cards-silhouette"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={MargaritaCards}
+      alt="Margarita's Cards"
+      className="margarita-cards"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={PhilBombSilhouette}
+      alt="Phil's bomb silhouette"
+      className="phil-bomb-silhouette"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={PhilBomb}
+      alt="Phil's Bomb"
+      className="phil-bomb"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={MickeySkullSilhouette}
+      alt="Mickey's skull silhouette"
+      className="mickey-skull-silhouette"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+    {/* Mickey */}
+    <motion.img
+      src={MickeySkull}
+      alt="Mickey's skull"
+      className="mickey-skull"
+      // initial={{ y: 50, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ delay: 1.375, duration: 0.375, ease: 'easeOut' }}
+    />
+
+    </div>
+
     </div>
   );
 };
