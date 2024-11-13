73b8080b999f1b2b4b3b8dc60dc204bebb4a223b
kavinchandar-alt <kavinchandar@hotmail.com>
2024-11-12 20:11:11 -0600

    Revert "setting up page3"
    This reverts commit 507dd7bec395a4e003e773445ecfc41e38fbde9a.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Page1 from './pages/Page1'; // Importing Page1 
-import Page3 from './pages/Page3';
+import Page1 from './pages/Page1'; // Importing Page1 component
 
 function App() {
   return (
     <div className="App">
       <Page1 />  {/* Rendering Page1 */}
-      <Page3 /> {/* Rendering Page3 */}
     </div>
   );
 }
