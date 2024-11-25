581359d9e100276a963768b5a5e86b109f9526e1
anjalimasur <amasur@usc.edu>
2024-11-24 19:51:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Page1 from './pages/Page1'; // Importing Page1 component
+import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
 
 function App() {
   return (
     <div className="App">
       <Page1 />  {/* Rendering Page1 */}
+      <Page2 />  {/* Rendering Page2 */}
       <Page3 /> {/* Rendering Page3 */}
     </div>
   );
