f484b80e924af24e5a51b611e92aad29fd9a08eb
kavinchandar-alt <kavinchandar@hotmail.com>
2024-11-28 16:39:40 -0600

    set up page 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Page1 from './pages/Page1'; // Importing Page1 component
 import Page3 from './pages/Page3';
+import Page4 from './pages/Page4';
 
 function App() {
   return (
     <div className="App">
       <Page1 />  {/* Rendering Page1 */}
       <Page3 /> {/* Rendering Page3 */}
+      <Page4 /> {/* Rendering Page4 */}
     </div>
   );
 }
